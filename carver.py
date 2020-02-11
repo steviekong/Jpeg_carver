8e05648403d1d6e8b2bfa102fa8a1e6a48970fa9
steviekong <sidharth.reji@gmail.com>
2020-02-11 10:05:19 -0500

    it finally works praise the lord

--- carver.py
+++ carver.py
@@ -8,7 +8,7 @@ def main():
 		print("Invalid input, you must specifiy a file as the first argument.")
 		exit(0)
 	readFile(sys.argv[1])
-
+# Reads file and creates the list of SOI AND EOI markers
 def readFile(filename):
 	startMarkerArr = []
 	endMarkerArr = []
@@ -31,46 +31,44 @@ def readFile(filename):
 	print("Found markers")
 	pairs = findPairs(startMarkerArr, endMarkerArr, sosArr)
 	buildFile(pairs)
-
+#Finds EIO
 def findStart(byte1, byte2):
 	if byte1 == b'\xFF' and byte2 == b'\xD8':
 		return True
 	return False
-
+#Finds SOI
 def findEnd(byte1, byte2):
 	if byte1 == b'\xFF' and byte2 == b'\xD9':
 		return True
 	return False
+
 def findSOS(byte1, byte2):
 	if byte1 == b'\xFF' and byte2 == b'\xDA':
 		return True
 	return False
-
+#Creates the pairs of SOI and EOI markers 
 def findPairs(startMarkerArr, endMarkerArr, sosArr):
 	markerPairs = []
 	for startI in range(0, len(startMarkerArr)):
 		for endI in range(0, len(endMarkerArr)):
-			if startMarkerArr[startI] > endMarkerArr[endI]:
+			if startMarkerArr[startI] < endMarkerArr[endI] + 2:
 				markerPairs.append((startMarkerArr[startI], endMarkerArr[endI]))
 				#for sosI in range(0, len(sosArr)):
-					#if sosArr[sosI] > startMarkerArr[startI] and sosArr[sosI] > endMarkerArr[endI]:
+					#if sosArr[sosI] > startMarkerArr[startI] and sosArr[sosI] < endMarkerArr[endI]:
 						#markerPairs.append((startMarkerArr[startI], endMarkerArr[endI]))
 	print("Found pairs size is " + str(len(markerPairs)))
 	return markerPairs
 
-
+#Tests all pairs and deletes invalid images using Pillow/ PIL
 def buildFile(markerPairs):
-	jpegBytes = bytes()
 	file = open(sys.argv[1], 'rb')
+	byteBuffer = file.read()
 	counter = 0
 	while counter < len(markerPairs):
-		file.seek(markerPairs[counter][0])
-		while markerPairs[counter][1] + 2 - markerPairs[counter][0] > 0:
-			byte = file.read(1)
-			jpegBytes += byte
-			markerPairs[counter][1] -= 1
-		if len(jpegBytes) == 0:
-			exit(0)
+		jpegBytes = bytearray()
+		start = markerPairs[counter][1]
+		jpegBytes.extend(byteBuffer[markerPairs[counter][0]:markerPairs[counter][1]+2])
+		print("Image size is " + str(len(jpegBytes)))
 		name = str(random.random())
 		jpegFile = open(name + ".jpg", 'wb+')
 		jpegFile.write(jpegBytes)
@@ -78,8 +76,8 @@ def buildFile(markerPairs):
 			Image.open(name + ".jpg")
 		except IOError:
 			os.remove(name + ".jpg")
-			continue
-		jpegBytes = bytes()
+			print("Invalid image removed")
+		counter += 1
 
 
 if __name__ == '__main__':
