bb5da9a49351392d9b138590963d30050383a744
steviekong <sidharth.reji@gmail.com>
2020-03-01 16:55:15 -0500

    final

--- carver.py
+++ carver.py
@@ -4,16 +4,17 @@ import random
 from PIL import Image
 
 '''
-Simple image carver. Right now it will assemble any and all JPEGS found including partial fragmented files.
+Simple image carver. Right now it will assemble any and all JPEGS found including partial fragmented files. It does not find the rest of the file.
 You must have pillow installed. You can do that by `pip install pillow`.
 
-YOU MUST HAVE PYTHON 3 NOT 2! THE Pillow version used is 3 and I can't guarantee any of this works on python 2.
+YOU MUST HAVE PYTHON 3 NOT 2! THE Pillow version used is the python version 3 and I can't guarantee any of this works on python 2.
 '''
 def main():
 	if len(sys.argv) < 2:
 		print("Invalid input, you must specify a file as the first argument.")
 		exit(0)
 	readFile(sys.argv[1])
+
 # Reads file and creates the list of SOI AND EOI markers
 def readFile(filename):
 	startMarkerArr = []
@@ -21,21 +22,19 @@ def readFile(filename):
 	sosArr = []
 	counter = 0
 	fileSize = os.stat(filename).st_size
-	with open(filename, 'rb') as file:
-		byte1 = file.read(1)
-		byte2 = file.read(1)
-		while counter <= fileSize:
-			if findStart(byte1, byte2):
+	file = open(filename, 'rb')
+	fileBuffer = bytearray(file.read())
+	while counter < fileSize:
+		byte1 = bytes([fileBuffer[counter]])
+		byte2 = bytes([fileBuffer[counter+1]])
+		if findStart(byte1, byte2):
 				startMarkerArr.append(counter)
-			if findEnd(byte1, byte2):
-				endMarkerArr.append(counter)
-			counter += 2
-			byte1 = file.read(1)
-			byte2 = file.read(1)
+		if findEnd(byte1, byte2):
+			endMarkerArr.append(counter)
+		counter += 2
 	print("Found markers")
 	pairs = findPairs(startMarkerArr, endMarkerArr, sosArr)
 	validCount = buildFile(pairs)
-	print("Total number of valid images found are " + str(validCount))
 
 #Finds SOI
 def findStart(byte1, byte2):
@@ -59,11 +58,12 @@ def findPairs(startMarkerArr, endMarkerArr, sosArr):
 	return markerPairs
 
 #Tests all pairs and tests/ deletes invalid images using Pillow/ PIL
+# Also tests to see if the discovered file is the smallest of the ones generated from the same SOI 
 def buildFile(markerPairs):
 	file = open(sys.argv[1], 'rb')
 	byteBuffer = file.read()
-	validCount = 0
-	counter = 0
+	counter = 0 
+	smallestHashMap = {}
 	while counter < len(markerPairs):
 		jpegBytes = bytearray()
 		start = markerPairs[counter][1]
@@ -77,10 +77,19 @@ def buildFile(markerPairs):
 			os.remove(name + ".jpg")
 			print("Invalid image removed")
 		else:
-			validCount += 1
-			print("File saved, the size is " + str(len(jpegBytes)))
+			if smallestHashMap.get(markerPairs[counter][0]) != None:
+				print(len(jpegBytes), smallestHashMap[markerPairs[counter][0]][0])
+			if counter != 0 and smallestHashMap.get(markerPairs[counter][0]) != None and len(jpegBytes) < smallestHashMap[markerPairs[counter][0]][0]:
+				print("Smaller image found, duplicate removed!")
+				os.remove(smallestHashMap[markerPairs[counter][0]][1])
+				smallestHashMap[markerPairs[counter][0]] = (len(jpegBytes), name + ".jpg")
+			if smallestHashMap.get(markerPairs[counter][0]) != None and len(jpegBytes) > smallestHashMap[markerPairs[counter][0]][0]:
+				os.remove(name + ".jpg")
+				print("Original is the smallest duplicate removed")
+			if smallestHashMap.get(markerPairs[counter][0]) == None:
+				smallestHashMap[markerPairs[counter][0]] = (len(jpegBytes), name + ".jpg")
+			print("One valid image has been added or replaced")
 		counter += 1
-	return validCount
-
+# No idea what this does 
 if __name__ == '__main__':
 	main()
